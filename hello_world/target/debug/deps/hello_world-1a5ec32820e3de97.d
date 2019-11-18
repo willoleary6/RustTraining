75946980794b7030af954b3f7b6892275ee7b18c
William <willoleary6@gmail.com>
2019-11-18 20:32:54 +0000

    Hello world set up and running

---
+++ hello_world/target/debug/deps/hello_world-1a5ec32820e3de97.d
@@ -0,0 +1,5 @@
+C:\SourceCode\RustTraining\hello_world\target\debug\deps\hello_world-1a5ec32820e3de97.exe: src\main.rs
+
+C:\SourceCode\RustTraining\hello_world\target\debug\deps\hello_world-1a5ec32820e3de97.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\SourceCode\RustTraining\hello_world\target\debug\hello_world.exe: C:\SourceCode\RustTraining\hello_world\src\main.rs
